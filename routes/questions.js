aceb053b315f08d1e645ff2abd13cc82316d8703
AmritGrewal001 <grewal13amrit@gmail.com>
2019-03-28 22:42:54 -0500

    CRUD Functions Added

--- routes/questions.js
+++ routes/questions.js
@@ -8,4 +8,44 @@ router.get('/', (req, res, next) => {
     });
 });
 
+
+//get reqquests to create question
+router.get('/create', (req, res, next) => {
+   res.render('question-create', {}); 
+});
+
+//post requests to create question
+router.post('/create', (req, res, next) => {
+   const createdQuestion = new Question(req.title);
+   createdQuestion.save().then(() => {
+      res.redirect('/questions'); 
+   });
+});
+
+
+//to handle editing
+router.get('/edit/:id', (req, res, next) => {
+   Question.findById(req.params.id).then(requestedQuestion => {
+       res.render('questions-edit', { question: requestedQuestion });
+   });
+});
+
+//handle post requests to edit question
+router.post('/edit/:id', (req, res, next) => {
+  const id = req.params.id;  
+  const title = req.title;    
+  Question.findByIdAndUpdate(id, title, {new: true}).then(() => {
+     res.redirect('/questions'); 
+  });
+});
+
+
+//to handle editing
+router.get('/delete/:id', (req, res, next) => {
+   const id = req.params.id;  
+   Question.findByIdAndDelete(id).then(() => {
+     res.redirect('/questions'); 
+   });
+});
+
 module.exports = router;
